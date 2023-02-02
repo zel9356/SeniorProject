3c39e2dc2386b0e94a9948a6d44bc567fa9a7bb3
zoeel <zel9356@rit.edu>
2023-02-01 19:32:07 -0500

    Correct dw graph code

--- DensityWeightedkNN.py
+++ DensityWeightedkNN.py
@@ -76,24 +76,29 @@ def create_kNN(data, k):
     return gauss_weighted_graph, unweighted_graph
 
 
-
 def AdaptiveThreshold(gauss_weighted_graph, k):
+    """
+    Based on the z score, finds which pixels fall where within the adpaptive threshold
+    :param gauss_weighted_graph: the Gaussian weighted graph
+    :param k: the number of neighbors each pixel is connected to
+    :return: list containing lists of pixels within each threshold, the final adapted threshold, and the codensity
+    """
+
     # calculate codensity
-    sum_dist = np.sum(gauss_weighted_graph, axis=1)
-    codensity = sum_dist / k
+    codensity = np.sum(gauss_weighted_graph, axis=1) / k
 
     # assign adaptive threshold based on z-score of codensity
     # decide adaptive k values via z-score of normal distribution
-    z_score = [-2, -1, 0, +1, +2]
-
+    z_score_list = [-2, -1, 0, +1, +2]
+    z_score = np.array(z_score_list)
     mean = np.mean(codensity)
     std = np.std(codensity)
 
-    adapt_thresh = [i * std + mean for i in z_score]
+    adapt_thresh = z_score * std + mean
 
     # introduce a normalization factor by bias
     bias = adapt_thresh[-1] / codensity.max()
-
+    print("adapt thresh: " + str(adapt_thresh))
     adapt_thresh = adapt_thresh / bias
 
     # adjust the high-end boundary of adapt_thresh
@@ -103,92 +108,28 @@ def AdaptiveThreshold(gauss_weighted_graph, k):
     adapt_thresh[0] = codensity.min() + (adapt_thresh[1] - codensity.min()) / 2
 
     # get number of Adaptive_Kpix
+    print("adapt thresh: " + str(adapt_thresh))
 
     # threshold_1, in the low density region
-    k_1 = np.where(codensity < adapt_thresh[0])
-    print(len(k_1[0]))
+    region_1 = np.where(codensity < adapt_thresh[0])
+    region_6 = np.where(codensity > adapt_thresh[-1])
+    region_2 = np.where(np.logical_and(codensity >= adapt_thresh[0], codensity < adapt_thresh[1]))
+    region_3 = np.where(np.logical_and(codensity >= adapt_thresh[1], codensity < adapt_thresh[2]))
+    region_4 = np.where(np.logical_and(codensity >= adapt_thresh[2], codensity < adapt_thresh[3]))
+    region_5 = np.where(np.logical_and(codensity >= adapt_thresh[3], codensity <= adapt_thresh[4]))
 
-    # threshold_2: interval
-    k_2left = np.where(codensity > adapt_thresh[0])
-
-    dict_k2left = {}
-    for i in range(len(k_2left[0])):
-        dict_k2left[k_2left[0][i]] = codensity[k_2left[0][i]]
-
-    k_2right = np.where(codensity < adapt_thresh[1])
-
-    dict_k2right = {}
-    for i in range(len(k_2right[0])):
-        dict_k2right[k_2right[0][i]] = codensity[k_2right[0][i]]
-
-    dict_k2 = dict_k2left.keys() & dict_k2right.keys()
-    dict_k2 = np.sort(list(dict_k2))  # convert set to list and then sort
-
-    print(len(dict_k2))
-
-    # threshold_3: interval
-    k_3left = np.where(codensity > adapt_thresh[1])
-
-    dict_k3left = {}
-    for i in range(len(k_3left[0])):
-        dict_k3left[k_3left[0][i]] = codensity[k_3left[0][i]]
-
-    k_3right = np.where(codensity < adapt_thresh[2])
-
-    dict_k3right = {}
-    for i in range(len(k_3right[0])):
-        dict_k3right[k_3right[0][i]] = codensity[k_3right[0][i]]
-
-    dict_k3 = dict_k3left.keys() & dict_k3right.keys()
-    dict_k3 = np.sort(list(dict_k3))  # convert set to list and then sort
-
-    print(len(dict_k3))
-
-    # threshold_4: interval
-    k_4left = np.where(codensity > adapt_thresh[2])
-
-    dict_k4left = {}
-    for i in range(len(k_4left[0])):
-        dict_k4left[k_4left[0][i]] = codensity[k_4left[0][i]]
-
-    k_4right = np.where(codensity < adapt_thresh[3])
-
-    dict_k4right = {}
-    for i in range(len(k_4right[0])):
-        dict_k4right[k_4right[0][i]] = codensity[k_4right[0][i]]
-
-    dict_k4 = dict_k4left.keys() & dict_k4right.keys()
-    dict_k4 = np.sort(list(dict_k4))  # convert set to list and then sort
-
-    print(len(dict_k4))
-
-    # threshold_5: interval
-    k_5left = np.where(codensity > adapt_thresh[3])
-
-    dict_k5left = {}
-    for i in range(len(k_5left[0])):
-        dict_k5left[k_5left[0][i]] = codensity[k_5left[0][i]]
-
-    k_5right = np.where(codensity < adapt_thresh[4])
-
-    dict_k5right = {}
-    for i in range(len(k_5right[0])):
-        dict_k5right[k_5right[0][i]] = codensity[k_5right[0][i]]
-
-    dict_k5 = dict_k5left.keys() & dict_k5right.keys()
-    dict_k5 = np.sort(list(dict_k5))  # convert set to list and then sort
-
-    print(len(dict_k5))
-
-    # threshold_6: interval
-    k_6 = np.where(codensity > adapt_thresh[4])
-
-    print(len(k_6[0]))
-
-    return (k_1[0], dict_k2, dict_k3, dict_k4, dict_k5, k_6[0]), adapt_thresh, codensity
+    return (region_1[0], region_2[0], region_3[0], region_4[0], region_5[0], region_6[0]), adapt_thresh, codensity
 
 
-def AdaptiveGW(GW, Kpixel, OriW):
+def AdaptiveGW(gauss_weighted_graph, pixels_threshold, unweighted_graph):
+    """
+
+    :param gauss_weighted_graph: the Gaussian weighted graph adjacency matrix
+    :param pixels_threshold: list containing lists of pixels within each threshold
+    :param unweighted_graph: unweighted adjacency matrix graph
+    :return:
+    """
+
     # define adaptive k values (number of neighbors) based on your scenario
     k_1max = 5  # k_max,here max means distance max, lower density
     k_2 = 6
@@ -198,43 +139,47 @@ def AdaptiveGW(GW, Kpixel, OriW):
     k_6min = 15
 
     # assign adaptive k-values for KNN graph
-    NN = np.argsort(OriW)
-    NN_1max = NN[:, 1:k_1max + 1]
-    NN_2 = NN[:, 1:k_2 + 1]
-    NN_3 = NN[:, 1:k_3 + 1]
-    NN_4 = NN[:, 1:k_4 + 1]
-    NN_5 = NN[:, 1:k_5 + 1]
-    NN_6min = NN[:, 1:k_6min + 1]
 
-    num = len(OriW)
+    # sort unweighted graph
+    sorted_unweigh = np.argsort(unweighted_graph)
+    print(sorted_unweigh)
+    NN_1max = sorted_unweigh[:, 1:k_1max + 1]
+    print(NN_1max)
+    NN_2 = sorted_unweigh[:, 1:k_2 + 1]
+    NN_3 = sorted_unweigh[:, 1:k_3 + 1]
+    NN_4 = sorted_unweigh[:, 1:k_4 + 1]
+    NN_5 = sorted_unweigh[:, 1:k_5 + 1]
+    NN_6min = sorted_unweigh[:, 1:k_6min + 1]
+
+    num = len(unweighted_graph)
     adapt_GW = np.zeros((num, num))
 
     # depend on adaptive k
     for i in range(num):
-        if i in Kpixel[0]:  # i is in high density region
+        if i in pixels_threshold[0]:  # i is in high density region
             for j in range(num):
                 if j in NN_1max[i]:
-                    adapt_GW[i][j] = GW[i, j]
-        elif i in Kpixel[1]:  # i is in average density region
+                    adapt_GW[i][j] = gauss_weighted_graph[i, j]
+        elif i in pixels_threshold[1]:  # i is in average density region
             for j in range(num):
                 if j in NN_2[i]:
-                    adapt_GW[i][j] = GW[i, j]
-        elif i in Kpixel[2]:  # i is in average density region
+                    adapt_GW[i][j] = gauss_weighted_graph[i, j]
+        elif i in pixels_threshold[2]:  # i is in average density region
             for j in range(num):
                 if j in NN_3[i]:
-                    adapt_GW[i][j] = GW[i, j]
-        elif i in Kpixel[3]:  # i is in average density region
+                    adapt_GW[i][j] = gauss_weighted_graph[i, j]
+        elif i in pixels_threshold[3]:  # i is in average density region
             for j in range(num):
                 if j in NN_4[i]:
-                    adapt_GW[i][j] = GW[i, j]
-        elif i in Kpixel[4]:  # i is in average density region
+                    adapt_GW[i][j] = gauss_weighted_graph[i, j]
+        elif i in pixels_threshold[4]:  # i is in average density region
             for j in range(num):
                 if j in NN_5[i]:
-                    adapt_GW[i][j] = GW[i, j]
+                    adapt_GW[i][j] = gauss_weighted_graph[i, j]
         else:  # i is in low density region
             for j in range(num):
                 if j in NN_6min[i]:
-                    adapt_GW[i][j] = GW[i, j]
+                    adapt_GW[i][j] = gauss_weighted_graph[i, j]
     return adapt_GW
 
 
@@ -251,20 +196,19 @@ def display(adj_mat):
 
 
 def main():
-
     if len(sys.argv) != 2:
         print("Command Line Arguments: How many bands?")
         return
     else:
         band_number = int(sys.argv[1])
-        coord_col, coord_row, reflect, lineNum = read_spectra_csv('filename.csv', band_number)
+        coord_col, coord_row, reflect, lineNum = read_spectra_csv('100.csv', band_number)
         reflect = reflect.reshape(lineNum,
                                   band_number)
         print(reflect.shape)
-        GW, W = create_kNN(reflect, 20)
-        Kpixel, thresh, coden = AdaptiveThreshold(GW, 20)
-        adapt_GW = AdaptiveGW(GW, Kpixel, W)
-        # adapt_GW.shape
+        gauss_weighted_graph, unweighted_graph = create_kNN(reflect, 20)
+        pixels_threshold, thresh, coden = AdaptiveThreshold(gauss_weighted_graph, 20)
+        adapt_GW = AdaptiveGW(gauss_weighted_graph, pixels_threshold, unweighted_graph)
+
         # add self-connection
         adapt_GW2 = adapt_GW + np.eye(adapt_GW.shape[0])
         display(adapt_GW2)
